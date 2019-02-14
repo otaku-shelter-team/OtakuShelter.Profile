c21104bbc915d4e767fda93831b77655ff1f55dc
Sergey <sergeyshaykhullin@gmail.com>
2019-02-14 16:59:10 +0300

    1.0.0 Change admin routes

--- src/OtakuShelter.Profile.Web/Profiles/ProfilesControllerRoutes.cs
+++ src/OtakuShelter.Profile.Web/Profiles/ProfilesControllerRoutes.cs
@@ -6,42 +6,42 @@ namespace OtakuShelter.Profile
 	{
 		public static IRoutingBuilder AddProfilesController(this IRoutingBuilder builder)
 		{
-			builder.AddController<ProfilesController>("profiles", controller =>
+			builder.AddController<ProfilesController>(controller =>
 			{
-				controller.AddRoute(c => c.Create(From.Body<CreateProfileViewModel>()))
+				controller.AddRoute("profiles", c => c.Create(From.Body<CreateProfileViewModel>()))
 					.HttpPost()
 					.Authorize();
 
-				controller.AddRoute(c => c.Read())
+				controller.AddRoute("profiles", c => c.Read())
 					.HttpGet()
 					.Authorize();
 
-				controller.AddRoute("{accountId}", c => c.ReadById(From.Route<int>()))
+				controller.AddRoute("profiles/{accountId}", c => c.ReadById(From.Route<int>()))
 					.HttpGet()
 					.Authorize();
 
-				controller.AddRoute(c => c.Update(From.Body<UpdateProfileViewModel>()))
+				controller.AddRoute("profiles", c => c.Update(From.Body<UpdateProfileViewModel>()))
 					.HttpPut()
 					.Authorize();
 
-				controller.AddRoute(c => c.Delete(From.Any<DeleteProfileViewModel>()))
+				controller.AddRoute("profiles", c => c.Delete(From.Any<DeleteProfileViewModel>()))
 					.HttpDelete()
 					.Authorize();
 
-				controller.AddRoute("admin", c => c.AdminRead(From.Query<FilterViewModel>()))
+				controller.AddRoute("admin/profiles", c => c.AdminRead(From.Query<FilterViewModel>()))
 					.HttpGet()
 					.Authorize("admin");
 
-				controller.AddRoute("admin/{profileId}", c => c.AdminReadById(From.Route<int>()))
+				controller.AddRoute("admin/profiles/{profileId}", c => c.AdminReadById(From.Route<int>()))
 					.HttpGet()
 					.Authorize("admin");
 
-				controller.AddRoute("admin/{profileId}",
+				controller.AddRoute("admin/profiles/{profileId}",
 						c => c.AdminUpdateById(From.Body<AdminUpdateByIdProfileViewModel>(), From.Route<int>()))
 					.HttpPut()
 					.Authorize("admin");
 
-				controller.AddRoute("admin/{profileId}", c => c.AdminDeleteById(From.Route<AdminDeleteByIdProfileViewModel>()))
+				controller.AddRoute("admin/profiles/{profileId}", c => c.AdminDeleteById(From.Route<AdminDeleteByIdProfileViewModel>()))
 					.HttpDelete()
 					.Authorize("admin");
 			});
