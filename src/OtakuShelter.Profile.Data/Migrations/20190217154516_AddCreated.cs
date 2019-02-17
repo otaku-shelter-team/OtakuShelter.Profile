3bab255024cffc5cd49857edbdd28c603ea1defc
Sergey <sergeyshaykhullin@gmail.com>
2019-02-17 18:45:32 +0300

    1.0.0 Add Created Migration

---
+++ src/OtakuShelter.Profile.Data/Migrations/20190217154516_AddCreated.cs
@@ -0,0 +1,23 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace OtakuShelter.Profile.Migrations
+{
+    public partial class AddCreated : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "createddatetimeutc",
+                table: "profiles",
+                newName: "created");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.RenameColumn(
+                name: "created",
+                table: "profiles",
+                newName: "createddatetimeutc");
+        }
+    }
+}
