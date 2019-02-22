3debf61f07364be2afe9a087a59395f247518220
Sergey <sergeyshaykhullin@gmail.com>
2019-02-23 00:02:21 +0300

    Test new deploy pipeline

--- deploy.sh
+++ deploy.sh
@@ -1,6 +1,8 @@
-PREVIOUS_VERSION=`expr $TRAVIS_BUILD_NUMBER - 1`
-
-echo docker pull otakushelter/profile:1.0.$TRAVIS_BUILD_NUMBER
-echo docker rm \$\(docker stop \$\(docker ps -a -q --filter ancestor=otakushelter/profile:1.0.$PREVIOUS_VERSION --format="{{.ID}}"\)\)
-echo docker rmi otakushelter/profile:1.0.$PREVIOUS_VERSION -f 
-echo docker run -d -p 127.0.0.1:4003:80 otakushelter/profile:1.0.$TRAVIS_BUILD_NUMBER
+echo "ansible-playbook deploy.yml \
+-e \"\
+otakushelter_hosts=profiles \
+otakushelter_container=otakushelter_profile \
+otakushelter_image=otakushelter/profile \
+otakushelter_port=4003 \
+otakushelter_build_number=$TRAVIS_BUILD_NUMBER\" \
+-i inventories/staging"
