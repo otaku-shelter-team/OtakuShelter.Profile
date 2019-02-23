fb39379f7986267b878f319f64a06eaa204cf41f
Sergey <sergeyshaykhullin@gmail.com>
2019-02-23 13:03:06 +0300

    1.0.0

--- deploy.sh
+++ deploy.sh
@@ -6,5 +6,6 @@ otakushelter_container=otakushelter_profile \
 otakushelter_image=otakushelter/profile \
 otakushelter_port=4003 \
 otakushelter_build_number=$TRAVIS_BUILD_NUMBER \
-otakushelter_database_password=$OTAKUSHELTER_DATABASE_PASSWORD\" \
+otakushelter_database_password=$OTAKUSHELTER_DATABASE_PASSWORD \
+otakushelter_secret=$OTAKUSHELTER_SECRET\" \
 -i inventories/staging"
